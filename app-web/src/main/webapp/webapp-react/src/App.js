fd86eb3f0e066f31bd68886c7c0822d144dad8c8
BNarbona <banarbona@gmail.com>
2022-07-29 18:49:10 -0300

    React - Intro 2

--- app-web/src/main/webapp/webapp-react/src/App.js
+++ app-web/src/main/webapp/webapp-react/src/App.js
@@ -1,76 +1,17 @@
-import { useState } from 'react';
-import './App.css';
-import DiasDeSemana from './Components/DiasDeSemana';
-import PrimerComponente from './Components/PrimerComponente';
-
-const dias = ['LUNES','MARTES','MIERCOLES']
-
+import styles from './App.module.css';
+import {MovieGrid} from '.components/MovieGrid';
 
 function App() {
-  //estado local del componente
-  const [edad,setEdad] = useState(0);
-  //edad=10; no se puede
-  //setEdad(150); no se puede
-  const [valor,setValor] = useState('');
-  
-  const incrementar = () => {
-      console.log('edad:',edad)
-      setEdad(edad+1);
-  }
-
-  const [diasSemana,setDiasSemana] = useState(dias);
-
-  const decrementar = () => {
-    if(edad>0){
-    setEdad(edad-1);
-    }
-}
-
-const cambiar = () => {
-  setEdad(valor);
-
-}
-
-const change = (newValor) => {
-  console.log(newValor);
-  setValor(newValor);
-
-}
+  // console.log(styles);
 
   return (
     <>
-      <div className = 'row'>
-            <div className='col-3'>
-              <label htmlFor='valor'>Edad: </label>
-              <input type='number' 
-                value={valor}
-                id='valor' 
-                onChange={(e) => change(e.target.value)}></input>
-              <label>Edad: {edad}</label>
-            <DiasDeSemana 
-              diasSemana = {diasSemana}
-              diaACtual = {'LUNES'}>
-            </DiasDeSemana>  
-            </div> 
-            <div className='col-3'>
-              <button className='btn btn-info' 
-                    onClick={incrementar}>
-                Incrementar Edad
-              </button>
-            </div>
-            <div className='col-3'>
-              <button className='btn btn-success' 
-                    onClick={decrementar}>
-                Decrementar Edad
-              </button>
-            </div>
-            <div className='col-3'>
-              <button className='btn btn-primary' 
-                    onClick={cambiar}>
-                Cambiar Edad
-              </button>
-            </div>  
-      </div>
+      <header>
+        <h1 className={styles.primerParrafo}>Peliculas</h1>
+      </header>
+      <main>
+        <MovieGrid></MovieGrid>
+      </main>
     </>
   );
 }
