5c9051a0cd8b8b5c51b8c1a9c122e0f1425ffe1b
BNarbona <banarbona@gmail.com>
2022-08-01 17:22:09 -0300

    React

--- app-web/src/main/webapp/webapp-react/src/App.js
+++ app-web/src/main/webapp/webapp-react/src/App.js
@@ -1,19 +1,29 @@
+import { BrowserRouter,Routes,Route, Link } from 'react-router-dom';
 import styles from './App.module.css';
 import { MovieGrid } from './components/MovieGrid';
-
+import { MovieDetail } from './components/MovieDetail';
+import { SearchMovie } from './components/SearchMovie';
 
 function App() {
   // console.log(styles);
 
   return (
-    <>
+    <BrowserRouter>
       <header>
-        <h1 className={styles.primerParrafo}>Peliculas</h1>
+        <Link to={'/'}>
+          <div>
+            <h1 className={styles.title}>Peliculas</h1>
+            <SearchMovie/>
+          </div>
+        </Link>
       </header>
       <main>
-        <MovieGrid></MovieGrid>
+        <Routes>
+          <Route path='/' element={<MovieGrid/>}/>
+          <Route path='/detail/:movieId' element={<MovieDetail/>}/>
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
   );
 }
 
